40f9954be627f06bdc33a5b25c1ae50d3f426b3e
Yuki Hirakawa <yuuki.hirakawa@ntt.com>
2020-07-16 21:43:05 +0900

    Initial Commit

---
+++ testApp202007/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testApp202007
+//
+//  Created by user1 on 2020/07/16.
+//  Copyright © 2020 Yuki Hirakawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
