9d44fe99ab45a2aeceba50fe88dca2c64a44edcd
Yuki Hirakawa <yuuki.hirakawa@ntt.com>
2020-07-20 14:42:49 +0900

    これでできたかな

---
+++ Pods/Target Support Files/Pods-testApp202007/Pods-testApp202007-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testApp202007 : NSObject
+@end
+@implementation PodsDummy_Pods_testApp202007
+@end
--- testApp202007/ViewController.swift
+++ testApp202007/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var chartView: LineChartView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let sales = [3205.0, 3120.0, 3424.0, 3312.0, 3612.0, 3594.0, 3648.0, 38454.0, 3585.0, 3917.0, 4014.0]
+        setChart(values: sales)
+    }
+    
+    func setChart(values: [Double]) {
+        var entry = [ChartDataEntry]()
+        
+        for i in 0..<values.count {
+            entry.append(ChartDataEntry(x: Double(i), y: values[i]))
+        }
+            
+        let dataSet = LineChartDataSet(entries: entry, label: "2020年度売上推移")
+        chartView.data = LineChartData(dataSet: dataSet)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet var myLabel: UILabel!
+    
+    @IBAction func buttonTapped(sender: UIButton) {
+        myLabel.text = "hello Worlddddddd!"
     }
 
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//    }
 
 }
 
